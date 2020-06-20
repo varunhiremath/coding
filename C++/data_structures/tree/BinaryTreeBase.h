df2549ecfb8ebe1157baa56ee6fb8272325dda2a
Varun Hiremath <varun@debian.org>
2020-06-20 02:42:56 -0700

    Auto adjust printing binary tree to number width

--- C++/data_structures/tree/BinaryTreeBase.h
+++ C++/data_structures/tree/BinaryTreeBase.h
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <iomanip>
+#include <cmath>
 
 template <typename T>
 class Node
@@ -45,28 +46,42 @@ public:
     printLeftView(node->right(), out);
   }
 
-  void prettyPrintTree(std::vector<Node<T>*> const &nodes, size_t const level, size_t const height, size_t const numwidth) const
+  void prettyPrintTree(std::vector<Node<T>*> const &nodes,
+                       size_t const level, size_t const height, size_t const numwidth) const
   {
     if (level == height)
       return;
 
-    size_t const indentSpace = 10;
-    size_t const leadingSpace = 1 << (height - level);
+    size_t bumpUp = 0;
+    int innerBranchSpace = 0;
+    do
+    {
+      ++bumpUp;
+      size_t const leadingSpace = (1 << bumpUp);
+      size_t const midSpace = 2*leadingSpace - 1;
+      innerBranchSpace = midSpace - (2*(numwidth-1));
+    }
+    while (innerBranchSpace < 1);
+
+    int spacingLevel = level - (bumpUp - 1);
+    size_t const indentSpace = 1;
+    size_t const leadingSpace = (1 << (height - spacingLevel));
     size_t const midSpace = 2*leadingSpace - 1;
     size_t const branchSpace = midSpace - (2*(numwidth-1));
 
     std::cout<<std::endl<<std::string(indentSpace+leadingSpace-numwidth, ' ');
 
     std::vector<Node<T>*> nextNodes;
-    bool leftSide = true;
+    bool leftNode = true;
+
+    std::string blankSpace = "";
     for (auto const& node : nodes)
     {
+      std::cout<<blankSpace;
       if (node)
       {
-        if (leftSide)
-          std::cout<<std::right<<std::setw(numwidth)<<node->data();
-        else
-          std::cout<<std::left<<std::setw(numwidth)<<node->data();
+        auto align = leftNode? std::right : std::left;
+        std::cout<<align<<std::setw(numwidth)<<node->data();
 
         nextNodes.push_back(node->left());
         nextNodes.push_back(node->right());
@@ -78,12 +93,8 @@ public:
         nextNodes.push_back(nullptr);
       }
 
-      if (leftSide)
-        std::cout<<std::string(midSpace, ' ');
-      else
-        std::cout<<std::string(branchSpace, ' ');
-
-      leftSide = !leftSide;
+      blankSpace = leftNode? std::string(midSpace, ' ') : std::string(branchSpace, ' ');
+      leftNode = !leftNode;
     }
 
     if (level+1 == height)
@@ -92,48 +103,30 @@ public:
       return;
     }
 
-    std::cout<<std::endl<<std::string(indentSpace+leadingSpace-numwidth, ' ');
-    leftSide = true;
-    for (auto const& node : nodes)
-    {
-      if (node && (node->left() || node->right()))
-      {
-        if (leftSide)
-          std::cout<<std::right<<std::setw(numwidth)<<"|";
-        else
-          std::cout<<std::left<<std::setw(numwidth)<<"|";
-      }
-      else
-      {
-        std::cout<<std::string(numwidth,' ');
-      }
-
-      if (leftSide)
-        std::cout<<std::string(midSpace, ' ');
-      else
-        std::cout<<std::string(branchSpace, ' ');
-
-      leftSide = !leftSide;
-    }
-
     size_t const nextLevel = level+1;
-    size_t const nextLeadingSpace = 1 << (height - nextLevel);
+    int const nextSpacingLevel = spacingLevel + 1;
+    size_t const nextLeadingSpace = 1 << (height - nextSpacingLevel);
     size_t nextMidSpace = 2*nextLeadingSpace-1;
 
     std::cout<<std::endl<<std::string(indentSpace+nextLeadingSpace, ' ');
 
+    blankSpace = "";
     for (size_t k = 0; k < (1 << nextLevel); k += 2)
     {
+      std::cout<<blankSpace;
       if (nextNodes[k] && nextNodes[k+1])
       {
         std::cout<<" ";
-        std::cout<<std::string(nextMidSpace-2, '-');
+        std::cout<<std::string(nextMidSpace/2-1, '_');
+        std::cout<<"|";
+        std::cout<<std::string(nextMidSpace/2-1, '_');
         std::cout<<" ";
       }
       else if (nextNodes[k])
       {
         std::cout<<" ";
-        std::cout<<std::string(nextMidSpace/2, '-');
+        std::cout<<std::string(nextMidSpace/2-1, '_');
+        std::cout<<"|";
         std::cout<<std::string(nextMidSpace/2-1, ' ');
         std::cout<<" ";
       }
@@ -141,7 +134,8 @@ public:
       {
         std::cout<<" ";
         std::cout<<std::string(nextMidSpace/2-1, ' ');
-        std::cout<<std::string(nextMidSpace/2, '-');
+        std::cout<<"|";
+        std::cout<<std::string(nextMidSpace/2-1, '_');
         std::cout<<" ";
       }
       else
@@ -149,29 +143,25 @@ public:
         std::cout<<std::string(nextMidSpace, ' ');
       }
 
-      std::cout<<std::string(nextMidSpace+2, ' ');
+      blankSpace = std::string(nextMidSpace+2, ' ');
     }
 
     std::cout<<std::endl<<std::string(indentSpace+nextLeadingSpace, ' ');
 
-    leftSide = true;
+    leftNode = true;
+    blankSpace = "";
     for (auto const& node : nextNodes)
     {
-      std::string const line = (leftSide)? "/":"\\";
-      if (node)
-      {
-        std::cout<<line;
-      }
-      else
-      {
-        std::cout<<" ";
-      }
+      std::cout<<blankSpace;
 
-      if (leftSide)
-        std::cout<<std::string(nextMidSpace-2, ' ');
+      std::string const slash = (leftNode)? "/":"\\";
+      if (node)
+        std::cout<<slash;
       else
-        std::cout<<std::string(nextMidSpace+2, ' ');
-      leftSide = !leftSide;
+        std::cout<<" ";
+
+      blankSpace = leftNode? std::string(nextMidSpace-2, ' ') : std::string(nextMidSpace+2, ' ');
+      leftNode = !leftNode;
     }
 
     prettyPrintTree(nextNodes, level+1, height, numwidth);
@@ -180,8 +170,10 @@ public:
   friend std::ostream &operator<<(std::ostream &out, BinaryTreeBase<T> const &bt)
   {
     size_t const height = bt.height(bt._head);
+    T const maxVal = bt.max(bt._head);
+    size_t const digits = std::min(4, 1 + static_cast<int>(std::floor(std::log10(maxVal))));
     std::vector<Node<T>*> nodes{bt._head};
-    bt.prettyPrintTree(nodes, 0, height, 2);
+    bt.prettyPrintTree(nodes, 0, height, digits);
     return out;
   }
 
@@ -192,6 +184,17 @@ protected:
       return 0;
     else
       return 1 + std::max(height(node->left()), height(node->right()));
+  }
+
+  T const max(Node<T>* const &head) const
+  {
+    if (!head) return 0;
+    Node<T>* node = head;
+    while (node->right())
+    {
+      node = node->right();
+    }
+    return node->data();
   }
 
   Node<T>* _head;
--- C++/data_structures/tree/testBinarySearchTree.cpp
+++ C++/data_structures/tree/testBinarySearchTree.cpp
@@ -5,25 +5,24 @@ int
 main()
 {
   BinarySearchTree<int> bst;
-  bst.insert(25);
-  bst.insert(20);
-  bst.insert(36);
-  bst.insert(10);
-  bst.insert(22);
-  bst.insert(30);
-  bst.insert(40);
-  bst.insert(5);
-  bst.insert(12);
-  bst.insert(28);
-  bst.insert(38);
-  bst.insert(48);
-  bst.insert(1);
-  bst.insert(8);
-  bst.insert(15);
-  bst.insert(45);
+  bst.insert(250);
+  bst.insert(200);
+  bst.insert(360);
+  bst.insert(100);
+  bst.insert(220);
+  bst.insert(300);
+  bst.insert(400);
   bst.insert(50);
-  bst.insert(23);
-  bst.insert(11);
+  bst.insert(120);
+  bst.insert(280);
+  bst.insert(380);
+  bst.insert(480);
+  bst.insert(10);
+  bst.insert(80);
+  bst.insert(150);
+  bst.insert(450);
+  bst.insert(500);
+  bst.insert(230);
 
   std::cout<<"BST = " << bst << std::endl;
 
